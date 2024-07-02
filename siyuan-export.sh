594ca83544a1eca13d78cc255086c7927369e4f7
Sagar Behere <1622959+sagarbehere@users.noreply.github.com>
2024-07-02 11:59:06 +0800

    Added auth token env var to siyuan-export.sh

--- siyuan-export.sh
+++ siyuan-export.sh
@@ -4,6 +4,6 @@ NOTES_DIR=notes/
 
 rm -rf logs/
 python3 create-index-files.py $NOTES_DIR
-python3 process-siyuan-links.py $NOTES_DIR
+AUTHTOKEN=setmefillmechangeme python3 process-siyuan-links.py $NOTES_DIR
 python3 add-backlinks.py $NOTES_DIR
 python3 copy-images.py $NOTES_DIR $NOTES_DIR/assets
